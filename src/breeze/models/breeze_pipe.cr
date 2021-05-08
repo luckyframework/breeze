2117c368b02ecc17554ecd6ce7f95c3f6a6dee7a
Jeremy Woertink <jeremywoertink@gmail.com>
2021-05-08 15:08:35 -0700

    Adding new position column to pipes so we can see if they are ran before or after

--- src/breeze/models/breeze_pipe.cr
+++ src/breeze/models/breeze_pipe.cr
@@ -2,6 +2,7 @@ class Breeze::BreezePipe < Breeze::BreezeBaseModel
   table :breeze_pipes do
     column name : String
     column continued : Bool
+    column position : String
     belongs_to breeze_request : BreezeRequest
   end
 end
