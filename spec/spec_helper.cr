415c22db127f6a5fee0e62f833c24d4bb9954dc1
Jeremy Woertink <jeremywoertink@gmail.com>
2021-05-20 13:42:42 -0700

    SPECS! (#45)
    * Adding in the first integration spec. Fixes #3
    
    * need all of the same stuff in CI as LuckyCli does.
    
    * needed yarn
    
    * fixing yarn setup in CI
    
    * Added spec assertion that the breeze install works
    
    * For some reason, lucky db.drop doesn't run... cleanup spec fails
    
    * needs postgres client too
    
    * still trying to get postgres setup for testing
    
    * also need chrome for flow
    
    * fixing postgres client
    
    * I have no idea why this spec keeps failing ☹️

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,9 +1,4 @@
 require "spec"
-
-# TODO: running `crystal spec` throws this error
-# In lib/avram/src/avram/database.cr:138:70
-#  138 | @@db || Avram::Connection.new(url, database_class: self.class).open
-#                                                                       ^---
-# Error: BUG: no target defs
-
-# require "../src/breeze"
+require "file_utils"
+require "lucky_cli"
+require "./support/**"
---
+++ spec/support/with_project_cleanup.cr
@@ -0,0 +1,19 @@
+module WithProjectCleanup
+  private def with_project_cleanup(project_directory = "test-project", skip_db_drop = false)
+    yield
+
+    FileUtils.cd(project_directory) {
+      output = IO::Memory.new
+      result = Process.run(
+        "lucky db.drop",
+        output: output,
+        shell: true
+      )
+
+      result.normal_exit?.should eq(true)
+      output.to_s.should contain("Done dropping")
+    } unless skip_db_drop
+  ensure
+    FileUtils.rm_rf project_directory
+  end
+end
