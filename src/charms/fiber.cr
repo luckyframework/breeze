fd6e969076b73c2db8fc0e43d69705e5ebe0604d
Liberatys <nick.flueckiger@renuo.ch>
2021-07-10 21:59:21 +0200

    Implement elapsed_text display for request/response

--- src/charms/fiber.cr
+++ src/charms/fiber.cr
@@ -3,4 +3,5 @@ class Fiber
   # This is stored in the Fiber so other stuff can access it.
   # For example, we can associate queries and pipes with the request
   property breeze_request : Breeze::BreezeRequest?
+  property breeze_request_start : Time::Span?
 end
