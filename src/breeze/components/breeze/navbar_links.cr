3eddf0a4516555bef8b0dc8c1040c11d3225fef8
Jeremy Woertink <jeremywoertink@gmail.com>
2021-08-22 11:28:33 -0700

    Forgot to remove a context instance in #50. (#55)

--- src/breeze/components/breeze/navbar_links.cr
+++ src/breeze/components/breeze/navbar_links.cr
@@ -3,11 +3,11 @@ class Breeze::NavbarLinks < Breeze::BreezeComponent
     mount_link("Requests", to: Breeze::Requests::Index)
     mount_link("Queries", to: Breeze::Queries::Index)
     Breeze.extensions.each do |extension|
-      mount_instance extension.navbar_link(context)
+      mount_instance extension.navbar_link
     end
   end
 
   private def mount_link(link_text : String, to action : Lucky::Action.class)
-    mount(Breeze::NavbarLink, context: context, link_text: link_text, link_to: action.path)
+    mount(Breeze::NavbarLink, link_text: link_text, link_to: action.path)
   end
 end
