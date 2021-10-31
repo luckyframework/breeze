fced9901778b4ea43d64bd1a440e55f6c6f9e113
Jeremy Woertink <jeremywoertink@gmail.com>
2021-10-31 15:00:28 -0700

    Push the lookup logic to the query object. Added a little note

--- src/breeze/actions/requests/show.cr
+++ src/breeze/actions/requests/show.cr
@@ -1,17 +1,10 @@
 class Breeze::Requests::Show < Breeze::BreezeAction
   get "/requests/:request_id" do
-    breeze_request = if request_id == "last"
-                       breeze_request_query.created_at.asc_order.last
-                     else
-                       breeze_request_query.find(request_id)
-                     end
-    html ShowPage, breeze_request: breeze_request
-  end
-
-  private def breeze_request_query : BreezeRequestQuery
-    BreezeRequestQuery.new
+    breeze_request = BreezeRequestQuery.new
       .preload_breeze_response
       .preload_breeze_sql_statements
       .preload_breeze_pipes
+      .find_specific_or_last(request_id)
+    html ShowPage, breeze_request: breeze_request
   end
 end
--- src/breeze/queries/breeze_request_query.cr
+++ src/breeze/queries/breeze_request_query.cr
@@ -1,3 +1,15 @@
 class Breeze::BreezeRequestQuery < Breeze::BreezeRequest::BaseQuery
   include Breeze::Queries::Resetable
+
+  # Pass in the string "last" to return the
+  # most recent BreezeRequest. Or pass in the ID
+  # of the specific BreezeRequest record you're
+  # looking for
+  def find_specific_or_last(id_or_last : String) : BreezeRequest
+    if id_or_last == "last"
+      created_at.asc_order.last
+    else
+      find(id_or_last)
+    end
+  end
 end
