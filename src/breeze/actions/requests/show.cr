8d27d50c3e08cf3a8403a090c6362f34d48406fd
Jeremy Woertink <jeremywoertink@gmail.com>
2021-10-31 14:51:12 -0700

    Shortcut to show the most recent request. Fixes #62

--- src/breeze/actions/requests/show.cr
+++ src/breeze/actions/requests/show.cr
@@ -1,10 +1,17 @@
 class Breeze::Requests::Show < Breeze::BreezeAction
   get "/requests/:request_id" do
-    breeze_request = BreezeRequestQuery.new
+    breeze_request = if request_id == "last"
+                       breeze_request_query.created_at.asc_order.last
+                     else
+                       breeze_request_query.find(request_id)
+                     end
+    html ShowPage, breeze_request: breeze_request
+  end
+
+  private def breeze_request_query : BreezeRequestQuery
+    BreezeRequestQuery.new
       .preload_breeze_response
       .preload_breeze_sql_statements
       .preload_breeze_pipes
-      .find(request_id)
-    html ShowPage, breeze_request: breeze_request
   end
 end
