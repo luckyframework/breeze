5d99be00d7477ba7d1747bdf14ddacf51e153670
Jeremy Woertink <jeremywoertink@gmail.com>
2022-09-30 10:46:59 -0700

    Shortcut to show the most recent request (#64)
    * Shortcut to show the most recent request. Fixes #62
    
    * Push the lookup logic to the query object. Added a little note

--- src/breeze/actions/requests/show.cr
+++ src/breeze/actions/requests/show.cr
@@ -4,7 +4,7 @@ class Breeze::Requests::Show < Breeze::BreezeAction
       .preload_breeze_response
       .preload_breeze_sql_statements
       .preload_breeze_pipes
-      .find(request_id)
+      .find_specific_or_last(request_id)
     html ShowPage, breeze_request: breeze_request
   end
 end
