61d0a80ac1b9e57bbb6675343bfa6d5331d93ef1
Jeremy Woertink <jeremywoertink@gmail.com>
2025-06-05 14:59:41 -0700

    formatting for the lastest crystal

--- src/breeze/pages/queries/show_page.cr
+++ src/breeze/pages/queries/show_page.cr
@@ -8,8 +8,8 @@ class Breeze::Queries::ShowPage < Breeze::BreezeLayout
   def content
     mount Breeze::Panel do
       mount Breeze::DescriptionList,
-        heading_title: ->{ text "Query details" },
-        list: ->{
+        heading_title: -> { text "Query details" },
+        list: -> {
           mount Breeze::DescriptionListRow, "Statement", breeze_sql_statement.statement
           mount Breeze::DescriptionListRow, "Args", breeze_sql_statement.args || "[]"
           mount Breeze::DescriptionListRow, "Model", breeze_sql_statement.model || "Unknown"
